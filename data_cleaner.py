8ff15b24de9a6a9d5d13137c8642a88502f24bcf
Alen <alen@Alens-MacBook-Pro.local>
2025-01-23 18:14:27 -0800

    update cleanup script for more error handling and create ipython notebook

--- data_cleaner.py
+++ data_cleaner.py
@@ -7,11 +7,19 @@ OUTPUT_FILENAME = "house_data.csv"
 TRAINING = "training_data.csv"
 TESTING = "testing_data.csv"
 
+def validate_data_columns(data):
+    for i in range(2, 12):
+        if not data[i] or data[i] == "":
+            return False
+
+    return True
+
 def split_training_testing(source_filename):
     source = DIRECTORY_PATH + "/" + source_filename
     with open(source, "r") as f:
         lines = f.readlines()
 
+    lines = lines[1:]
     data_count = len(lines)
     training_data_count = round(data_count * 0.8)
 
@@ -38,6 +46,7 @@ def shuffle_data(source_filename, destination_filename):
 
     destination = DIRECTORY_PATH + "/" + destination_filename
     with open(destination, "w") as f:
+        f.write("PROPERTY TYPE,ADDRESS,CITY,ZIP OR POSTAL CODE,PRICE,BEDS,BATHS,LOCATION,SQUARE FEET\n")
         f.writelines(lines)
 
     # remove temporary file because we shuffle data and write to house_data.csv
@@ -48,21 +57,36 @@ def parse_data(filenames, destination_filename, directory_path):
     output_fp = open(destination, "w")
 
     for filename in filenames:
+        if not filename.startswith("redfin_"):
+            continue
+
         source_filename = directory_path + "/" + filename
         print("Parsing file: {}".format(filename))
         with open(source_filename, "r") as fp:
             line = fp.readline().strip()
             while line:
                 data = line.split(",")
-                
+          
                 # this is noise data, drop it off
                 if len(data) < 27:
                     line = fp.readline().strip()
 
                     continue
 
-                # we are just interested in column 2 - 13
-                output_fp.write(",".join(data[2:14]) + "\n")
+                # remove header
+                if data[0] == "SALE TYPE":
+                    line = fp.readline().strip()
+
+                    continue
+
+                # remove the line if it has empty
+                if not validate_data_columns(data):
+                    line = fp.readline().strip()
+
+                    continue
+
+                # we are just interested in column 2 - 11
+                output_fp.write(",".join(data[2:5] + data[6:12]) + "\n")
                 line = fp.readline().strip()
 
     output_fp.close()
